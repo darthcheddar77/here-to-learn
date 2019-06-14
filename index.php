9217798365509a1d080e2b1a3ccfbd72f4249396
grant <darthcheddar77>
2019-06-14 12:48:27 -0700

    almost done

--- index.php
+++ index.php
@@ -74,9 +74,26 @@ $users = $statement->fetchALL();
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Document</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 
 <body>
+
+<ul class="nav justify-content-center">
+  <li class="nav-item">
+    <a class="nav-link active" href="index.php">Home</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="admin.php">Admin</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="php.php">php guide</a>
+  </li>
+ </ul>
+
+
+
+
   <form method="POST" action="">
     <div class="form-group">
       <label for="email">Email address</label>
